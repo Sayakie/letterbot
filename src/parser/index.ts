f768901185260e0804dd547eaf012f26b06a4536
Sayakie <shuvi.dev.b701@gmail.com>
2020-04-13 12:14:26 +0900

    Add try-catch block to find error object

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -2,16 +2,20 @@ import * as weather from './weather';
 import * as news from './news';
 import * as date from './date';
 
-const parser = async() => {
-  const weatherContent = await weather.parse();
-  const newsContent = await news.parse();
-  const dateContent = date.parser();
-
-  return {
-    weather: weatherContent,
-    news: newsContent,
-    date: dateContent,
-  };
+const parser = async () => {
+  try {
+    const weatherContent = await weather.parse();
+    const newsContent = await news.parse();
+    const dateContent = date.parser();
+  
+    return {
+      weather: weatherContent,
+      news: newsContent,
+      date: dateContent,
+    };
+  } catch (err: any) {
+    console.error(`Errot occured at parser: ${err}`)
+  }
 };
 
 export default parser;
