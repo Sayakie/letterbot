10377dad3c005a0ad0bb5fc1453a65d6f5046f7c
Sayakie <shuvi.dev.b701@gmail.com>
2020-04-17 03:48:27 +0900

    Update index.ts

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -3,19 +3,15 @@ import * as news from './news';
 import * as date from './date';
 
 const parser = async () => {
-  try {
-    const weatherContent = await weather.parse();
-    const newsContent = await news.parse();
-    const dateContent = date.parser();
+  const weatherContent = await weather.parse();
+  const newsContent = await news.parse();
+  const dateContent = date.parser();
   
-    return {
-      weather: weatherContent,
-      news: newsContent,
-      date: dateContent,
-    };
-  } catch (err: any) {
-    console.error(`Errot occured at parser: ${err}`)
-  }
+  return {
+    weather: weatherContent,
+    news: newsContent,
+    date: dateContent,
+  };
 };
 
 export default parser;
