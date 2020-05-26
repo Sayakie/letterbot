e21db6473a327a3b21a8e864a87c1c61ce462ef0
Sayakie <sayakie.ovo@gmail.com>
2020-05-26 11:13:48 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -175,16 +175,17 @@ const velogTrendUrl = 'https://velog.io/'
 
       if (hookUrl.includes('discordapp.com')) {
         const message: any = {
-          username: '감염된 부관',
-          // avatar_url: 'https://i.imgur.com/diJEYhI.jpg',
-          avatar_url: 'https://i.imgur.com/m1zwJWY.jpg',
+          username: '집행관',
+          // avatar_url: 'https://i.imgur.com/diJEYhI.jpg', // 테란 부관
+          // avatar_url: 'https://i.imgur.com/m1zwJWY.jpg', // 저그 감염된 부관
+          avatar_url: 'https://i.imgur.com/UrRo8HN.jpg',
           embeds: []
         }
 
         console.log(JSON.stringify(result, null, 2))
         message.embeds.push({
           color: config.embedColor,
-          description: `사냥하기 좋은 날입니다, 여왕님. ${today[2]}년 ${today[0]}월 ${today[1]}일 보고입니다.`,
+          description: `엔 타로 아둔! 집행관님! 칼라가 우릴 인도합니다. ${today[2]}년 ${today[0]}월 ${today[1]}일 보고입니다.`,
           fields: [{
             name: `🏞️ 날씨 / 목포`,
             value: result[0].weather,
